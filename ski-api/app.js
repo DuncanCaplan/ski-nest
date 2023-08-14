c46683a923fa138d781964fd1e548a731f764c46
DuncanCaplan <duncan.caplan@outlook.com>
2023-08-14 21:45:44 +0100

    first commit

---
+++ ski-api/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).send("Hello there");
+  } catch (error) {
+    res.status(500).send({ error: "Server error" });
+  }
+});
+
+module.exports = app;
